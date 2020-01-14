1f722d731a873bf19cec13d9117a9fc3f1915f07
jrg96 <gomezlopez.jorge96@gmail.com>
2020-01-14 09:17:37 -0600

    Agregado el metodo lista de album e insertar album

--- src/main/java/com/empresa/api/controller/AlbumController.java
+++ src/main/java/com/empresa/api/controller/AlbumController.java
@@ -1,27 +1,38 @@
 package com.empresa.api.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.empresa.api.entity.Album;
+import com.empresa.api.service.IAlbumService;
 
 @RestController
 @RequestMapping("/api/album")
 public class AlbumController 
 {
+	@Autowired
+	private IAlbumService albumService;
+	
 	@PreAuthorize("hasAnyRole('USUARIO', 'ADMIN')")
 	@GetMapping("/list")
-	public String obtenerAlbum()
+	public List<Album> obtenerAlbum()
 	{
-		return "Solo un usuario y admin pueden verme";
+		return this.albumService.buscarTodas();
 	}
 	
 	
 	@PreAuthorize("hasAnyRole('ADMIN')")
 	@PostMapping("/save")
-	public String insertarAlbum()
+	public Album insertarAlbum(@RequestBody Album album)
 	{
-		return "Solo un admin puede verme";
+		this.albumService.guardar(album);
+		return album;
 	}
 }
