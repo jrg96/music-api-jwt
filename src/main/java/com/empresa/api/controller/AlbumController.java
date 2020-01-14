5df5589760f95fa7d629cd53bfed79f15e341b49
jrg96 <gomezlopez.jorge96@gmail.com>
2020-01-14 08:31:12 -0600

    Asegurando recursos en base a rol

--- src/main/java/com/empresa/api/controller/AlbumController.java
+++ src/main/java/com/empresa/api/controller/AlbumController.java
@@ -1,16 +1,27 @@
 package com.empresa.api.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/album")
 public class AlbumController 
 {
-	@GetMapping("/albums")
+	@PreAuthorize("hasAnyRole('USUARIO', 'ADMIN')")
+	@GetMapping("/list")
 	public String obtenerAlbum()
 	{
-		return "Hola mundo";
+		return "Solo un usuario y admin pueden verme";
+	}
+	
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
+	@PostMapping("/save")
+	public String insertarAlbum()
+	{
+		return "Solo un admin puede verme";
 	}
 }
